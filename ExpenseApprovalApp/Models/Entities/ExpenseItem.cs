d25a516301c4c8742bcd92ea16c106b941aadb88
kaanismen <kaan.ismen@ozu.edu.tr>
2026-05-27 18:21:59 +0300

    Expense Item Class has initiated.

---
+++ ExpenseApprovalApp/Models/Entities/ExpenseItem.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseApprovalApp.Models.Enums;
+
+namespace ExpenseApprovalApp.Models.Entities
+{
+    public class ExpenseItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public ExpenseRequest ExpenseRequest { get; set; }
+        public int ExpenseRequestId { get; set; }
+        public ExpenseCategory Category { get; set; }
+        
+
+    }
+}
