cf1fefa9d01675b847af27ea1ead58fff922ac20
kaanismen <kaan.ismen@ozu.edu.tr>
2026-05-31 02:45:25 +0300

    Account Controler Login function and login cshtml initiated.

--- ExpenseApprovalApp/Controllers/AccountController.cs
+++ ExpenseApprovalApp/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using ExpenseApprovalApp.Models.Entities;
+using ExpenseApprovalApp.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,5 +16,30 @@ namespace ExpenseApprovalApp.Controllers
             _signInManager = signInManager;
         }
 
+        [HttpGet]
+        public IActionResult Login() { return View(); }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model) 
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid email veya şifre");
+                    return View(model);
+                }
+
+            } 
+            else
+            {
+                return View(model);
+            }
+        } 
     }
 }
