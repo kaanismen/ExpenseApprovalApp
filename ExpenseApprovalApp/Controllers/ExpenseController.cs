3a117085b782f6e3feecc19208d7979efd8d9a3b
kaanismenwrof <kaan.ismen@wroftech.com>
2026-06-01 14:26:10 +0300

    Expense Controller Initiated.

---
+++ ExpenseApprovalApp/Controllers/ExpenseController.cs
@@ -0,0 +1,20 @@
+﻿using ExpenseApprovalApp.Data;
+using ExpenseApprovalApp.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseApprovalApp.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly AppDbContext _dbContext;
+
+        public ExpenseController(UserManager<AppUser> userManager, AppDbContext dbContext)
+        {
+            _userManager = userManager;
+            _dbContext = dbContext;
+        }
+
+    }
+}
