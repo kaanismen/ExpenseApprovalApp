4c7d2af08524d1a1ff61a7a8ba0aed1402707821
kaanismen <kaan.ismen@ozu.edu.tr>
2026-06-10 14:28:24 +0300

    Expense Request ViewModel Polish

--- ExpenseApprovalApp/Models/ViewModels/ExpenseItemViewModel.cs
+++ ExpenseApprovalApp/Models/ViewModels/ExpenseItemViewModel.cs
@@ -1,11 +1,18 @@
 ﻿using ExpenseApprovalApp.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExpenseApprovalApp.Models.ViewModels
 {
     public class ExpenseItemViewModel
     {
+        [Required(ErrorMessage = "The expense request must have a title.")]
+        [StringLength(50, ErrorMessage = "Title must not exceed 100 characters.")]
         public string Name { get; set; }
-        public string Description { get; set; }
+
+        [StringLength(50, ErrorMessage = "Title must not exceed 100 characters.")]
+        public string? Description { get; set; }
+
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335", ErrorMessage = "Amount must be greater than 0.")]
         public decimal Amount { get; set; }
         public ExpenseCategory Category { get; set; }
     }
--- ExpenseApprovalApp/Models/ViewModels/ExpenseRequestViewModel.cs
+++ ExpenseApprovalApp/Models/ViewModels/ExpenseRequestViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace ExpenseApprovalApp.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseApprovalApp.Models.ViewModels
 {
     public class ExpenseRequestViewModel
     {
+        [Required(ErrorMessage = "The expense request must have a title.")]
+        [StringLength(100, ErrorMessage = "Title must not exceed 100 characters.")]
         public string Title { get; set; }
-        public string Description { get; set; }
+        [StringLength(500, ErrorMessage = "Description must not exceed 500 characters.")]
+        public string? Description { get; set; }
         public List<ExpenseItemViewModel> Items { get; set; }
     }
 }
